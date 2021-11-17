6865d89aadb5332e82d2ca095e875745554faeca
EugenKupriyanov <eugenkupriyanov@coherentsolutions.com>
2021-11-17 15:02:20 +0300

    remover spec file

--- src/app/components/amount-of-currency-input/amount-of-currency-input.component.spec.ts
+++ src/app/components/amount-of-currency-input/amount-of-currency-input.component.spec.ts
@@ -1,25 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AmountOfCurrencyInputComponent } from './amount-of-currency-input.component';
+// import { AmountOfCurrencyInputComponent } from './amount-of-currency-input.component';
 
-describe('AmountOfCurrencyInputComponent', () => {
-  let component: AmountOfCurrencyInputComponent;
-  let fixture: ComponentFixture<AmountOfCurrencyInputComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AmountOfCurrencyInputComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AmountOfCurrencyInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
